f024886889e27de312383133e94427394ac71ffe
JorritMontijn <jorritmontijn@gmail.com>
2023-01-11 11:27:56 +0100

    Update runPupilPlotsPSTH.m

--- NystagmusAlbino/runPupilPlotsPSTH.m
+++ NystagmusAlbino/runPupilPlotsPSTH.m
@@ -9,19 +9,18 @@
 %cellAreaGroupsAbbr = {'Ctx','NOT','Hip'};
 %cellSubjectGroups = {'BL6','DBA'};
 runHeaderNOT;
+intRunMaxArea = 2;
 
 vecColAlb = [0.9 0 0];
 vecColBl6 = lines(1);
 
 %% get data
 %pre-allocate
-vecPupilSTA_edges = [-0.5:0.02:0.5];
-cellAggX = {};
-cellAggY = {};
-cellAggSize = {};
-cellAggSync = {};
-cellAggOriIdx = {};
-cellSTA = cellfill(nan(0,numel(vecPupilSTA_edges)-1),[2 numel(cellAreaGroups)])
+dblMoveEventThreshold = 0.05;
+vecSTA_edges = [-4:0.1:4];
+cellSTA = cellfill(nan(0,numel(vecSTA_edges)-1),[3 intRunMaxArea 0]); %[move-type x V1/NOT x rec]
+matSTAZetaP = nan([3 intRunMaxArea 0]); %[move-type x V1/NOT x rec]
+cellZetaP = cell([3 intRunMaxArea 0]); %[move-type x V1/NOT x rec]
 
 %run
 cellNameAP = arrayfun(@(x) x.sJson.file_preproAP,sExp,'uniformoutput',false);
@@ -30,7 +29,7 @@ cellRemove = {};%{'RecMA5_2021-03-01R01_g0_t0'};
 indRemRecs = contains(cellExperiment,cellRemove);
 indRemRecs2 = ~contains(cellNameAP,cellUseForEyeTracking);
 cellSubjectType = arrayfun(@(x) x.sJson.subjecttype,sExp,'uniformoutput',false);
-for intSubType=1:2
+for intSubType=1
 	if intSubType == 1
 		strSubjectType = 'BL6';
 		dblOffsetT=0;
@@ -44,36 +43,11 @@ for intSubType=1:2
 	end
 	indUseRecs = contains(cellSubjectType,strSubjectType);
 	vecRunRecs = find(indUseRecs & ~(indRemRecs | indRemRecs2));
-	matAggTE_LocX = [];
-	matAggTE_LocY = [];
-	matAggTE_Size = [];
-	matAggTE_Sync = [];
-	vecTrialCounts = [];
-	matFracMoveRight = zeros(24,0);
-	matFracMoveUp = zeros(24,0);
-	vecAggOriIdx = [];
-	vecAggCounts = zeros(24,1);
+	
 	for intRecIdx=1:numel(vecRunRecs)
 		intRec=vecRunRecs(intRecIdx);
 		sRec = sExp(intRec);
 		strRec = sRec.Name(1:(end-7));
-		
-		%% pupil loc
-		vecPupilT = sRec.sPupil.vecTime;
-		vecPupilLocX = sRec.sPupil.vecCenterX;
-		vecPupilLocY = sRec.sPupil.vecCenterY;
-		vecPupilSize = sRec.sPupil.vecRadius;
-		vecPupilSync = sRec.sPupil.vecSyncLum;
-		if isfield(sRec.sPupil,'vecBlinks') && ~all(sRec.sPupil.vecBlinks==0)
-			vecPupilBlinks = sRec.sPupil.vecBlinks;
-		else
-			%filter absvidlum
-			dblLowPass = 0.01/(1/median(diff(vecPupilT)));
-			[fb,fa] = butter(2,dblLowPass,'high');
-			vecPupilAbsVidLum = zscore(filtfilt(fb,fa, sRec.sPupil.sRaw.vecPupilAbsVidLum));
-			vecPupilBlinks = vecPupilAbsVidLum > 5;
-		end
-		dblFs = 1/median(diff(vecPupilT));
 		
 		%% get stim absence
 		cellStimType = cellfun(@(x) x.strExpType,sRec.cellBlock,'uniformoutput',false);
@@ -104,6 +78,9 @@ for intSubType=1:2
 		
 		%get eye movement events
 		sPupil = sRec.sPupil;
+		if ~isfield(sPupil,'vecBlinks')
+			sPupil.vecBlinks = false(size(sPupil.vecTime));
+		end
 		dblPupilStart = dblBlankStart+dblPL+10;
 		dblPupilStop = dblBlankStart+dblLongestBlankPeriod+dblPL-10;
 		indUsePupil = sPupil.vecTime>dblPupilStart & sPupil.vecTime<dblPupilStop & ~sPupil.vecBlinks;
@@ -113,128 +90,205 @@ for intSubType=1:2
 		vecdT = diff(vecPupilT);
 		vecdX = diff(vecPupilX);
 		vecdY = diff(vecPupilY);
-		%vecdM = sqrt(vecdX.^2 + vecdY.^2)./vecdT;
-		vecdM = vecdX;
-		%vecdM = vecdY;
-		strMoveType = 'horz';
-		
-		%5% cut-off
-		[vecSortdM,vecReorder] = sort(vecdM);
-		int5P = floor(numel(vecSortdM)*0.05);
-		vecUpperValIdx = vecReorder((end-int5P-1):end);
-		indUpperVals = false(size(vecdM));
-		indUpperVals(vecUpperValIdx) = true;
-		vecUpperVals = vecdM(vecUpperValIdx);
-		
-		%get epochs
-		vecStartMoveIdx = 1+find(diff(indUpperVals)==1);
-		vecStopMoveIdx = find(diff(indUpperVals)==-1);
-		if vecStartMoveIdx(1) > vecStopMoveIdx(1)
-			vecStartMoveIdx = cat(2,1,vecStartMoveIdx);
-		end
-		if vecStartMoveIdx(end) > vecStopMoveIdx(end)
-			vecStopMoveIdx = cat(2,vecStopMoveIdx,vecStartMoveIdx(end));
-		end
-		vecMoveDurSecs = nan(size(vecStopMoveIdx));
-		for intEpoch=1:numel(vecMoveDurSecs)
-			vecMoveDurSecs(intEpoch) = sum(vecdT(vecStartMoveIdx(intEpoch):vecStopMoveIdx(intEpoch)));
-		end
-		dblMedianFrameDur = median(vecdT);
-		vecCutOffs = dblMedianFrameDur*(0.5+(1:(max(vecStopMoveIdx-vecStartMoveIdx)+1)));
-		vecStartMoveT = vecPupilT(vecStartMoveIdx)-dblPL;
-		
-		
-		%% spike-triggered average per area
-		indUseCells = arrayfun(@(x) x.Violations1ms < 0.25 & abs(x.NonStationarity) < 0.25,sRec.sCluster(:));
-		cellAreasPerCluster = {sRec.sCluster.Area};
-		for intArea=1:numel(cellUseAreas)
-			% select cells
-			strAreaGroup =  cellAreaGroupsAbbr{intArea};
-			vecSelectCells = find(indUseCells(:) & flat(contains(cellAreasPerCluster,cellUseAreas{intArea},'IgnoreCase',true)));
-			if isempty(vecSelectCells)
-				continue;
-			end
-			vecSpikeT = sort(cell2vec({sRec.sCluster(vecSelectCells).SpikeTimes}));
-			%reduce spikes to max 100k
-			intMaxSpikes = 100000; 
-			if numel(vecSpikeT) > intMaxSpikes
-				vecUseSpikes = unique(round(linspace(1,numel(vecSpikeT),intMaxSpikes)));
+		hFig=figure;
+		maxfig(hFig);
+		for intMoveType = 1:3
+			if intMoveType==1
+				vecdM = vecdX./vecdT;
+				strMoveType = 'horz';
+			elseif intMoveType==2
+				vecdM = vecdY./vecdT;
+				strMoveType = 'vert';
+			elseif intMoveType==3
+				vecdM = sqrt(vecdX.^2 + vecdY.^2)./vecdT;
+				strMoveType = 'tot';
 			else
-				vecUseSpikes = 1:numel(vecSpikeT);
+				error('not possible');
 			end
 			
-			%% plot grand average over all cells
-			doPEP(vecSpikeT(vecUseSpikes),[-4:0.05:4],vecStartMoveT);
-			intResampNum=250;
-			intPlot=0;
-			[dblZetaP,sZeta] = zetatest(vecSpikeT(vecUseSpikes),vecStartMoveT-2,4,intResampNum,intPlot);
-			ylabel(sprintf('Total firing rate in %s (Hz)',strAreaGroup));
-			xlabel(sprintf('Time after %s eye-movement (s)',strMoveType));
-			title(sprintf('%s (%s) - %s %s',strRec,strSubjectType,strAreaGroup,strMoveType),'interpreter','none');
+			%5% cut-off
+			[vecSortdM,vecReorder] = sort(vecdM);
+			intCutOff = floor(numel(vecSortdM)*dblMoveEventThreshold);
+			vecUpperValIdx = vecReorder((end-intCutOff-1):end);
+			indUpperVals = false(size(vecdM));
+			indUpperVals(vecUpperValIdx) = true;
+			vecUpperVals = vecdM(vecUpperValIdx);
 			
-			%% go through individual cells
-			error do this
+			%get epochs
+			vecStartMoveIdx = 1+find(diff(indUpperVals)==1);
+			vecStopMoveIdx = find(diff(indUpperVals)==-1);
+			if vecStartMoveIdx(1) > vecStopMoveIdx(1)
+				vecStartMoveIdx = cat(2,1,vecStartMoveIdx);
+			end
+			if vecStartMoveIdx(end) > vecStopMoveIdx(end)
+				vecStopMoveIdx = cat(2,vecStopMoveIdx,vecStartMoveIdx(end));
+			end
+			vecMoveDurSecs = nan(size(vecStopMoveIdx));
+			for intEpoch=1:numel(vecMoveDurSecs)
+				vecMoveDurSecs(intEpoch) = sum(vecdT(vecStartMoveIdx(intEpoch):vecStopMoveIdx(intEpoch)));
+			end
+			dblMedianFrameDur = median(vecdT);
+			vecCutOffs = dblMedianFrameDur*(0.5+(1:(max(vecStopMoveIdx-vecStartMoveIdx)+1)));
+			vecStartMoveT = vecPupilT(vecStartMoveIdx)-dblPL;
+			
+			%% spike-triggered average per area
+			indUseCells = arrayfun(@(x) x.Violations1ms < 0.25 & abs(x.NonStationarity) < 0.25,sRec.sCluster(:));
+			cellAreasPerCluster = {sRec.sCluster.Area};
+			for intArea=1:intRunMaxArea
+				% select cells
+				strAreaGroup =  cellAreaGroupsAbbr{intArea};
+				vecSelectCells = find(indUseCells(:) & flat(contains(cellAreasPerCluster,cellUseAreas{intArea},'IgnoreCase',true)));
+				if isempty(vecSelectCells)
+					continue;
+				end
+				cellSpikes = {sRec.sCluster(vecSelectCells).SpikeTimes};
+				intCellNum = numel(vecSelectCells);
+				vecSpikeT = sort(cell2vec(cellSpikes));
+				%reduce spikes to max 100k
+				intMaxSpikes = inf;
+				if numel(vecSpikeT) > intMaxSpikes
+					vecUseSpikes = unique(round(linspace(1,numel(vecSpikeT),intMaxSpikes)));
+				else
+					vecUseSpikes = 1:numel(vecSpikeT);
+				end
+				
+				%% plot grand average over all cells
+				figure(hFig);
+				hAx = subplot(2,3,intMoveType+(intArea-1)*3);
+				[vecMean,vecSEM,vecWindowBinCenters,matPET] = doPEP(vecSpikeT(vecUseSpikes),vecSTA_edges,vecStartMoveT,hAx);
+				intResampNum=250;
+				intPlot=0;
+				[dblZetaP,sZeta] = zetatest(vecSpikeT(vecUseSpikes),vecStartMoveT-2,4,intResampNum,intPlot);
+				%dblZetaP=nan;
+				ylabel(sprintf('Total firing rate in %s (Hz)',strAreaGroup));
+				xlabel(sprintf('Time after %s eye-movement (s)',strMoveType));
+				title(sprintf('%s (%s) - %s %s; Z-p=%.3e [N=%d cells]',strRec,strSubjectType,strAreaGroup,strMoveType,dblZetaP,intCellNum),'interpreter','none');
+				
+				%% go through individual cells
+				vecZetaP = nan(1,intCellNum);
+				for intCell=1:intCellNum
+					vecSpikes = cellSpikes{intCell};
+					intOrigId = vecSelectCells(intCell);
+					[dblZetaP,sZeta] = zetatest(vecSpikes,vecStartMoveT-2,4,intResampNum,intPlot);
+					vecZetaP(intCell) = dblZetaP;
+				end
+				
+				%% save data
+				cellSTA{intMoveType,intArea,intRecIdx} = matPET; %[move-type x V1/NOT x rec]
+				matSTAZetaP(intMoveType,intArea,intRecIdx) = dblZetaP; %[move-type x V1/NOT x rec]
+				cellZetaP{intMoveType,intArea,intRecIdx} = vecZetaP; %[move-type x V1/NOT x rec]
+			end
 		end
-	end
-	%save data
-	cellAggX{intSubType} = matAggTE_LocX;
-	cellAggY{intSubType} = matAggTE_LocY;
-	cellAggSize{intSubType} = matAggTE_Size;
-	cellAggSync{intSubType} = matAggTE_Sync;
-	cellAggOriIdx{intSubType} = vecAggOriIdx;
-end
-
-%% plot 1
-%define data
-vecOriRight = find(ismember(vecUnique,mod(-15:15:15,360)));
-vecOriLeft = find(ismember(vecUnique,mod(180 + [-15:15:15],360)));
-vecTime = vecWindowBinEdges(2:end) - median(diff(vecWindowBinEdges))/2;
-figure;maxfig;
-for intSubType=1:2
-	
-	matLeft = cellAggX{intSubType}(ismember(cellAggOriIdx{intSubType},vecOriLeft),:);
-	matLeft = matLeft - nanmean(matLeft,2);
-	matRight = cellAggX{intSubType}(ismember(cellAggOriIdx{intSubType},vecOriRight),:);
-	matRight = matRight - nanmean(matRight,2);
-	subplot(2,3,intSubType);
-	hold on
-	errorbar(vecTime,nanmean(matLeft,1),nanstd(matLeft,[],1)./sqrt(size(matLeft,1)-sum(isnan(matLeft),1)),'xb');
-	errorbar(vecTime,nanmean(matRight,1),nanstd(matRight,[],1)./sqrt(size(matRight,1)-sum(isnan(matRight),1)),'xr');
-	hold off
-	legend({'Leftward stimulus','Rightward stimulus'},'location','best');
-	ylabel('Normalized pupil X position');
-	xlabel('Time after stim onset (s)');
-	title(cellSubjectGroups{intSubType});
-	fixfig;
-end
-
-%% save 1
-drawnow;maxfig;
-export_fig(fullpath(strTargetPath,['EyeMovementLeftRight.tif']));
-export_fig(fullpath(strTargetPath,['EyeMovementLeftRight.pdf']));
-
-%% plot 2
-figure;maxfig;
-for intSubType=1:2
-	if intSubType == 2
-		vecCol = vecColAlb;
-	else
-		vecCol = vecColBl6;
-	end
-	for intArea=1:2
-		matSTA = zscore(cellSTA{intSubType,intArea},[],2);
-		subplot(2,3,intSubType+(intArea-1)*3);
 		
-		errorbar(vecPupilSTA_centers,mean(matSTA,1),std(matSTA,[],1)/sqrt(size(matSTA,1)),'color',vecCol);
-		title(sprintf('%s, %s',cellSubjectGroups{intSubType},cellAreaGroupsAbbr{intArea}));
-		xlabel('Time after spike (s)');
-		ylabel('Pupil x-movement (z-score)');
-		fixfig
+		%% save figure
+		drawnow;
+		strFigName = ['PupilMoveSpikePSTH_' strRec];
+		export_fig(fullpath(strTargetPath,[strFigName '.tif']));
+		export_fig(fullpath(strTargetPath,[strFigName '.pdf']));
 	end
 end
-%cellSTA{intSubType,intArea}
 
-%% save 2
-drawnow;maxfig;
-export_fig(fullpath(strTargetPath,['EyeMovementSTA.tif']));
-export_fig(fullpath(strTargetPath,['EyeMovementSTA.pdf']));
+%% save data
+cellNames = {sExp(vecRunRecs).Name};
+save(fullpath(strTargetPath,'PupilPSTH.mat'),'cellSTA','matSTAZetaP','cellZetaP','cellNames','vecSTA_edges');
+
+%% plot 1: cell-level
+%transform p2z
+cellZetaZ = cellfun(@(x) -norminv(x/2),cellZetaP,'UniformOutput',false);
+indRemRec = squeeze(any(any(cellfun(@isempty,cellZetaZ),2),1));
+cellZetaZ_red = cellZetaZ(:,:,~indRemRec);
+cellCompZ = cell(3,2);
+vecBinsE=0:0.5:6;
+vecBinsC = vecBinsE(2:end)-median(diff(vecBinsE));
+figure;maxfig;
+for intMoveType=1:3
+	subplot(2,3,intMoveType);
+	hold on;
+	if intMoveType==1
+		strMoveType = 'horz';
+	elseif intMoveType==2
+		strMoveType = 'vert';
+	elseif intMoveType==3
+		strMoveType = 'tot';
+	else
+		error('not possible');
+	end
+	for intArea=1:intRunMaxArea
+		if intArea==1
+			varCol = 'b';
+		elseif intArea==2
+			varCol = 'r';
+		else
+			error('not possible');
+		end
+		
+		cellCompZ{intMoveType,intArea} = cell2vec(cellZetaZ_red(intMoveType,intArea,:));
+		vecBinsV = histcounts(cellCompZ{intMoveType,intArea},vecBinsE);
+		plot(vecBinsC,vecBinsV./sum(vecBinsV),varCol);
+	end
+	xlabel('Z-score');
+	ylabel('Normalized fraction of cells');
+	title(sprintf('%s; spiking modulation by eye movement',strMoveType));
+	fixfig; grid off;
+	
+	%plot significant cells
+	subplot(2,3,intMoveType+3);
+	hold on;
+	dbl2Sd = -norminv(0.05/2);
+	dblAlphaSd = (1-normcdf(1))*2;
+	k1 = sum(cellCompZ{intMoveType,1}>dbl2Sd);
+	n1 = numel(cellCompZ{intMoveType,1});
+	k2 = sum(cellCompZ{intMoveType,2}>dbl2Sd);
+	n2 = numel(cellCompZ{intMoveType,2});
+	[phatV1,pciV1] = binofit(k1,n1,dblAlphaSd);
+	[phatNot,pciNot] = binofit(k2,n2,dblAlphaSd);
+	[pBino,z] = bino2test(k1,n1,k2,n2);
+	
+	errorbar(0.2,phatV1,phatV1-pciV1(1),phatV1-pciV1(2),'xb');
+	errorbar(0.8,phatNot,phatNot-pciNot(1),phatNot-pciNot(2),'xr');
+	xlim([0 1])
+	set(gca,'xtick',[0.2 0.8],'xticklabel',{'V1','NOT'});
+	ylabel('Fraction of significant cells (mean +/- sd)');
+	title(sprintf('%s; 2-sample binomial test, p=%.3f',strMoveType,pBino));
+	fixfig; grid off;
+	
+end
+
+%save fig
+drawnow;
+strFigName = ['PupilMoveCellModulation'];
+export_fig(fullpath(strTargetPath,[strFigName '.tif']));
+export_fig(fullpath(strTargetPath,[strFigName '.pdf']));
+
+%% plot 2: rec-level
+matAvg=cell2mat(cellfun(@(x) reshape(zscore(mean(x)),[1 1 1 numel(vecSTA_edges)-1]),cellSTA(:,:,~indRemRec),'UniformOutput',false));
+intUseRecs = sum(~indRemRec);
+matMean = squeeze(mean(matAvg,3));
+matSd = squeeze(std(matAvg,[],3));
+vecSTAC = vecSTA_edges(2:end)-mean(diff(vecSTA_edges))/2;
+figure;maxfig;
+for intMoveType=1:3
+	subplot(2,3,intMoveType);
+	hold on;
+	if intMoveType==1
+		strMoveType = 'horz';
+	elseif intMoveType==2
+		strMoveType = 'vert';
+	elseif intMoveType==3
+		strMoveType = 'tot';
+	else
+		error('not possible');
+	end
+	for intArea=1:intRunMaxArea
+		if intArea==1
+			varCol = [0 0 1];
+		elseif intArea==2
+			varCol = [1 0 0];
+		else
+			error('not possible');
+		end
+		
+		%plot
+		errorfill(vecSTAC,squeeze(matMean(intMoveType,intArea,:)),squeeze(matSd(intMoveType,intArea,:))./sqrt(intUseRecs),varCol);
+	end
+end

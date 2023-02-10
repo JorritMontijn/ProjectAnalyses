e3f4ada4008c769ba1afcf9fed86d61857b1c029
JorritMontijn <jorritmontijn@gmail.com>
2023-02-10 15:56:51 +0100

    Update runKoenKDE.m

--- runKoenKDE.m
+++ runKoenKDE.m
@@ -2,92 +2,129 @@
 intSizeN = 64; %size of grid: 32 for each neuron, 64 for standardized axes
 boolNormRunDens = true; %normalize for running density? (true)
 boolRemRun0 = false; %instead of normalizing for running density, you can also remove speed=0
-boolStandardizeAxes = true; %if true, you can sum all KDE maps over neurons, otherwise each neuron gets its own axis
-boolPlotPerNeuron = true; %make plot for every neuron?
-
-%% prep data
-%standardize axes?
-matAllData = signals;
-
-%remove zero run speed?
-if boolRemRun0
-	indRem = runspeed==0;
-else
-	indRem = false(size(runspeed));
-end
-
+boolPlotPerNeuron = false; %make plot for every neuron?
+boolRectifyAxes = true;
 %log-transform
-matAllData = log(matAllData(~indRem,:)+1);
-vecRunSpeed = log(runspeed(~indRem)+1);
+matAllData = log(signals+1);
+matRunSpeed = log(runspeed+1);
 
-%standardize axes?
-if boolStandardizeAxes
 %define range
-	vecMinXY = [min(vecRunSpeed(:)) min(matAllData(:))];
-	vecMaxXY = [max(vecRunSpeed(:)) max(matAllData(:))];
+if boolRectifyAxes
+	vecMinXY = [0 -0.2];
+	vecMaxXY = [3.3 1.1];
+else
+	vecMinXY = [min(matRunSpeed(:)) min(matAllData(:))];
+	vecMaxXY = [max(matRunSpeed(:)) max(matAllData(:))];
 end
 
-%% run
-for intNeuron=1:size(matAllData,2)
-	vecNeuronAct = matAllData(:,intNeuron);
+matAllDens = zeros(intSizeN, intSizeN, size(matAllData,2), 2);
+
+for i = 1:2
+	%% prep data
+	matSubData = matAllData(:,:,i);
+	vecRunSpeed = matRunSpeed(:,i);
 	
-	%make matrix
-	matData = cat(2,vecRunSpeed,vecNeuronAct);
-	
-	%define range
-	if ~boolStandardizeAxes
-		vecMinXY = min(matData,[],1);
-		vecMaxXY = max(matData,[],1);
+	%remove zero run speed?
+	if boolRemRun0
+		indRem = vecRunSpeed==0;
+	else
+		indRem = false(size(vecRunSpeed));
 	end
+	matSubData = matSubData(~indRem,:);
+	vecRunSpeed = vecRunSpeed(~indRem);
 	
-	%get 2D diffusion KDE
-	[bandwidth,matDens,X,Y]=kde2d(matData,intSizeN,vecMinXY,vecMaxXY);
 	
-	%normalize density for # of running speed samples
-	if boolNormRunDens
-		matDens = matDens ./ sum(matDens,1);
-	end
-	vecX = X(1,1:end);
-	vecY = Y(1:end,1);
-	vecLimX = [min(vecX) max(vecX)];
-	vecLimY = [min(vecY) max(vecY)];
-	
-	%% plot
-	if boolPlotPerNeuron
-		subplot(2,3,1)
-		histx(matData(:,1));
-		xlim(vecLimX);
-		xlabel('Running speed');
-		vecTickX = get(gca,'xtick');
-		vecRealX = exp(vecTickX)-1;
-		set(gca,'xticklabel',roundi(vecRealX,1));
-		fixfig;
+	%% run
+	for intNeuron=1:size(matSubData,2)
+		%% single neuron
+		vecNeuronAct = matSubData(:,intNeuron);
 		
-		subplot(2,3,2);
-		histx(matData(:,2));
-		xlim(vecLimY);
-		xlabel('dF/F0 (%)');
-		vecTickY = get(gca,'xtick');
-		vecRealY = exp(vecTickY)-1;
-		set(gca,'xticklabel',round(vecRealY*100));
-		fixfig;
+		%make matrix
+		matData = cat(2,vecRunSpeed,vecNeuronAct);
 		
+		%get 2D diffusion KDE
+		[bandwidth,matDens,X,Y]=kde2d(matData,intSizeN,vecMinXY,vecMaxXY);
 		
-		%define axes in real units
-		vecTickX = round(linspace(1,intSizeN,4));
-		vecTickX_val = roundi(exp(vecX(vecTickX))-1,1);
+		%normalize density for # of running speed samples
+		if boolNormRunDens
+			matDens = matDens ./ (sum(matDens,1));
+		end
+		vecX = X(1,1:end);
+		vecY = Y(1:end,1);
+		vecLimX = [vecMinXY(1) vecMaxXY(1)];
+		vecLimY = [vecMinXY(2) vecMaxXY(2)];
 		
-		vecTickY = round(linspace(1,intSizeN,4));
-		vecTickY_val = round(100*(exp(vecY(vecTickY))-1));
+		%save to output
+		matAllDens(:,:,intNeuron,i) = matDens;
 		
-		subplot(2,3,3);
-		imagesc(log(matDens+1));
-		set(gca,'xtick',vecTickX,'xticklabel',vecTickX_val,'ytick',vecTickY,'yticklabel',vecTickY_val);
-		ylabel('dF/F0 (%)');
-		xlabel('Running speed');
-		axis xy
-		title(sprintf('Neuron %d',intNeuron));
-		fixfig;
-		pause
+		% plot
+		if boolPlotPerNeuron
+			%define axes in real units
+			vecTickX = round(linspace(1,intSizeN,4));
+			vecTickX_val = roundi(exp(vecX(vecTickX))-1,1);
+			
+			vecTickY = round(linspace(1,intSizeN,4));
+			vecTickY_val = round(100*(exp(vecY(vecTickY))-1));
+			
+			
+			subplot(2,3,1);maxfig;
+			histx(matData(:,1));
+			xlim(vecLimX);
+			xlabel('Running speed');
+			drawnow;
+			set(gca,'xtick',roundi(log(vecTickX_val+1),1),'xticklabel',vecTickX_val);
+			fixfig;
+			
+			subplot(2,3,2);
+			histx(matData(:,2));
+			xlim(vecLimY);
+			xlabel('dF/F0 (%)');
+			set(gca,'xtick',roundi(log(vecTickY_val/100+1),1),'xticklabel',vecTickY_val);
+			fixfig;
+			
+			
+			
+			subplot(2,3,3);
+			imagesc(log(matDens+1));
+			colorbar;
+			set(gca,'xtick',vecTickX,'xticklabel',vecTickX_val,'ytick',vecTickY,'yticklabel',vecTickY_val);
+			ylabel('dF/F0 (%)');
+			xlabel('Running speed');
+			axis xy
+			title(sprintf('Neuron %d',intNeuron));
+			fixfig;
+			pause
+		end
 	end
 end
+
+%% plot overall fig
+figure;maxfig;
+vecTickX = round(linspace(1,intSizeN,4));
+vecTickX_val = roundi(exp(vecX(vecTickX))-1,1);
+
+vecTickY = round(linspace(1,intSizeN,4));
+vecTickY_val = round(100*(exp(vecY(vecTickY))-1));
+
+matDensDiff = diff(matAllDens,[],4);
+matDensDiffPyr = mean(matDensDiff(:,:,~reds),3);
+matDensDiffChand = mean(matDensDiff(:,:,reds),3);
+subplot(2,3,1)
+imagesc(log(matDensDiffPyr+1));
+colorbar;
+set(gca,'xtick',vecTickX,'xticklabel',vecTickX_val,'ytick',vecTickY,'yticklabel',vecTickY_val);
+ylabel('dF/F0 (%)');
+xlabel('Running speed');
+axis xy
+title(sprintf('Pyr, post-pre'));
+fixfig;
+
+subplot(2,3,2)
+imagesc(log(matDensDiffChand+1));
+colorbar;
+set(gca,'xtick',vecTickX,'xticklabel',vecTickX_val,'ytick',vecTickY,'yticklabel',vecTickY_val);
+ylabel('dF/F0 (%)');
+xlabel('Running speed');
+axis xy
+title(sprintf('Chand, post-pre'));
+fixfig;

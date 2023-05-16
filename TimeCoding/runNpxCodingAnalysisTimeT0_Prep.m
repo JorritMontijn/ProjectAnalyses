5519679999f494af85d62c915c3ad823c560f91a
JorritMontijn <jorritmontijn@gmail.com>
2023-05-16 17:23:03 +0200

    20230516

--- TimeCoding/runNpxCodingAnalysisTimeT0_Prep.m
+++ TimeCoding/runNpxCodingAnalysisTimeT0_Prep.m
@@ -81,8 +81,8 @@ for intRec=1:numel(sAggStim) %19 || weird: 11
 		% spikes at pop level, detect peaks, and split into trials.
 		%get spikes per trial per neuron
 		intTotS = sum(cellfun(@numel,cellSpikeTimes));
-		vecAllSpikeTime = nan(1,intTotS);
-		vecAllSpikeNeuron = zeros(1,intTotS,'int16');
+		vecAllSpikeTime_Real = nan(1,intTotS);
+		vecAllSpikeNeuron_Real = zeros(1,intTotS,'int16');
 		vecAllSpikeTime_Shuff = nan(1,intTotS);
 		vecAllSpikeNeuron_Shuff = zeros(1,intTotS,'int16');
 		intNumN = size(matMeanRate,1);
@@ -91,8 +91,8 @@ for intRec=1:numel(sAggStim) %19 || weird: 11
 			%add spikes
 			intThisS = numel(cellSpikeTimes{intN});
 			vecSpikeT = cellSpikeTimes{intN};
-			vecAllSpikeTime(intS:(intS+intThisS-1)) = vecSpikeT;
-			vecAllSpikeNeuron(intS:(intS+intThisS-1)) = intN;
+			vecAllSpikeTime_Real(intS:(intS+intThisS-1)) = vecSpikeT;
+			vecAllSpikeNeuron_Real(intS:(intS+intThisS-1)) = intN;
 			
 			%
 			vecISI = diff(sort(vecSpikeT));
@@ -105,9 +105,9 @@ for intRec=1:numel(sAggStim) %19 || weird: 11
 		dblMaxDur = vecOrigStimOnTime(end)-vecOrigStimOnTime(1)+dblStimDur+10;
 		
 		%sort
-		[vecAllSpikeTime,vecReorder] = sort(vecAllSpikeTime);
-		vecAllSpikeNeuron = vecAllSpikeNeuron(vecReorder);
-		[vecTime_Real,vecIFR_Real] = getIFR(vecAllSpikeTime,vecEvents,dblMaxDur,[],[],[],0); %takes about 1 minute
+		[vecAllSpikeTime_Real,vecReorder] = sort(vecAllSpikeTime_Real);
+		vecAllSpikeNeuron_Real = vecAllSpikeNeuron_Real(vecReorder);
+		[vecTime_Real,vecIFR_Real] = getIFR(vecAllSpikeTime_Real,vecEvents,dblMaxDur,[],[],[],0); %takes about 1 minute
 		vecTime_Real = vecTime_Real + vecEvents(1);
 		
 		%shuffled
@@ -116,115 +116,39 @@ for intRec=1:numel(sAggStim) %19 || weird: 11
 		[vecTime_Shuff,vecIFR_Shuff] = getIFR(vecAllSpikeTime_Shuff,vecEvents,dblMaxDur,[],[],[],0); %takes about 1 minute
 		vecTime_Shuff = vecTime_Shuff + vecEvents(1);
 		
-		%filter
+		%% filter
+		%real
 		intLag = round((numel(vecIFR_Real)/numel(vecOrigStimOnTime))/2);
 		dblThreshZ = 1.5;
 		dblInfluence = 0.05;
 		[signals,avgFilter_Real,stdFilter_Real] = detectpeaks(vecIFR_Real,intLag,dblThreshZ,dblInfluence);
 		%vecIFR_Real = (vecIFR_Real - avgFilter_Real)./stdFilter_Real;
-		vecIFR_Real = (vecIFR_Real - avgFilter_Real)./avgFilter_Real;
-		indRem_Real = isinf(vecIFR_Real);
-		vecIFR_Real(indRem_Real) = [];
-		vecTime_Real(indRem_Real) = [];
+		vecNormIFR_Real = (vecIFR_Real - avgFilter_Real)./avgFilter_Real;
+		indRem_Real = isinf(vecNormIFR_Real);
+		vecNormIFR_Real(indRem_Real) = [];
+		vecNormTime_Real  = vecTime_Real(~indRem_Real);
 		
 		%shuff
 		[signals,avgFilter_Shuff,stdFilter_Shuff] = detectpeaks(vecIFR_Shuff,intLag,dblThreshZ,dblInfluence);
 		%vecIFR_Shuff = (vecIFR_Shuff - avgFilter_Shuff)./stdFilter_Shuff;
-		vecIFR_Shuff = (vecIFR_Shuff - avgFilter_Shuff)./avgFilter_Shuff;
-		indRem_Shuff = isinf(vecIFR_Shuff);
-		vecIFR_Shuff(indRem_Shuff) = [];
-		vecTime_Shuff(indRem_Shuff) = [];
-		
-		%% go through trials
-		for intType=[]%1:2
-			if intType==1
-				vecTime = vecTime_Real;
-				vecIFR = vecIFR_Real;
-			else
-				vecTime = vecTime_Shuff;
-				vecIFR = vecIFR_Shuff;
-			end
-			figure;maxfig;
-			subplot(2,3,1)
-			hold on
-			dblBinDur = 0.05;
-			vecBinEdges = 0:dblBinDur:dblStimDur;
-			vecBinC = vecBinEdges(2:end)-dblBinDur/2;
-			intBinNum = numel(vecBinC);
-			matAvgIFR = nan(intStimNum,intBinNum);
-			[vecMean,vecSEM,vecWindowBinCenters,matAvgPET] = doPEP(vecTime,vecBinEdges,vecOrigStimOnTime,-1);
-			for intRep=1:intStimNum
-				indSpikesInTrial = vecTime>(vecOrigStimOnTime(intRep)-10) & vecTime<(vecOrigStimOnTime(intRep)+dblStimDur+10);
-				vecTrialTime = vecTime(indSpikesInTrial)-vecOrigStimOnTime(intRep);
-				vecTrialIFR = vecIFR(indSpikesInTrial);
-				try
-					matAvgIFR(intRep,:) = interp1(vecTrialTime,vecTrialIFR,vecBinC);
-				end
-				if intRep==50
-					plot(vecTrialTime,vecTrialIFR,'color',[0.5 0.5 0.5]);
-				end
-			end
-			xlim([0 dblStimDur]);
-			title(strRec,'interpreter','none');
-			xlabel('Time (s)');
-			ylabel('Raw IFR of trial #50');
-			
-			subplot(2,3,2)
-			errorbar(vecBinC,vecMean,vecSEM);
-			xlabel('Time (s)');
-			ylabel('Avg binned spiking rate');
-			
-			subplot(2,3,3)
-			errorbar(vecBinC,nanmean(matAvgIFR,1),nanstd(matAvgIFR,[],1)./sqrt(intStimNum));
-			xlabel('Time (s)');
-			ylabel('Avg binned IFR');
-			
-			% make distro plots
-			subplot(2,3,4)
-			histx(matAvgIFR(:));
-			xlabel('Binned IFR');
-			ylabel('# of bins');
-			
-			subplot(2,3,5)
-			histx(matAvgPET(:));
-			xlabel('Binned spike rates');
-			ylabel('# of bins');
-			
-			subplot(2,3,6)
-			vecBinsE = -1.2:0.1:2;
-			vecBinsC = vecBinsE(2:end)-median(diff(vecBinsE))/2;
-			vecC = histcounts(vecIFR(vecTime>(vecOrigStimOnTime(1)) & vecTime<(vecOrigStimOnTime(end)+dblStimDur)),vecBinsE);
-			plot(vecBinsC,vecC);
-			xlabel('Raw IFR');
-			ylabel('# of spikes');
-			if intType==1
-				vecHist_Real = vecC;
-			else
-				vecHist_Shuff = vecC;
-			end
-			
-		end
-		%figure
-		%subplot(2,3,1)
-		%plot(vecBinsC,vecHist_Real)
-		%hold on
-		%plot(vecBinsC,vecHist_Shuff)
-		%subplot(2,3,4)
-		%plot(vecBinsC,(vecHist_Real-vecHist_Shuff)./(vecHist_Real+vecHist_Shuff))
+		vecNormIFR_Shuff = (vecIFR_Shuff - avgFilter_Shuff)./avgFilter_Shuff;
+		indRem_Shuff = isinf(vecNormIFR_Shuff);
+		vecNormIFR_Shuff(indRem_Shuff) = [];
+		vecNormTime_Shuff = vecTime_Shuff(~indRem_Shuff);
 		
 		%% shuff peaks
-		indStimSpikes_Shuff = vecTime_Shuff>(vecOrigStimOnTime(1)-dblStimDur) & vecTime_Shuff<(vecOrigStimOnTime(end)+dblStimDur);
-		vecStimIFR_Shuff = vecIFR_Shuff(indStimSpikes_Shuff);
-		vecStimTime_Shuff = vecTime_Shuff(indStimSpikes_Shuff);
+		indStimSpikes_Shuff = vecNormTime_Shuff>(vecOrigStimOnTime(1)-dblStimDur) & vecNormTime_Shuff<(vecOrigStimOnTime(end)+dblStimDur);
+		vecStimIFR_Shuff = vecNormIFR_Shuff(indStimSpikes_Shuff);
+		vecStimTime_Shuff = vecNormTime_Shuff(indStimSpikes_Shuff);
 			
-		[pks_Shuff,locs_Shuff,w_Shuff,p_Shuff] = findpeaks(vecStimIFR_Shuff);
+		[vecPeakHeight_Shuff,vecPeakLocs_Shuff,w_Shuff,p_Shuff] = findpeaks(vecStimIFR_Shuff);
 			
 		%% real peaks
-		indStimSpikes_Real = vecTime_Real>(vecOrigStimOnTime(1)-dblStimDur) & vecTime_Real<(vecOrigStimOnTime(end)+dblStimDur);
-		vecStimIFR_Real = vecIFR_Real(indStimSpikes_Real);
-		vecStimTime_Real = vecTime_Real(indStimSpikes_Real);
+		indStimSpikes_Real = vecNormTime_Real>(vecOrigStimOnTime(1)-dblStimDur) & vecNormTime_Real<(vecOrigStimOnTime(end)+dblStimDur);
+		vecStimIFR_Real = vecNormIFR_Real(indStimSpikes_Real);
+		vecStimTime_Real = vecNormTime_Real(indStimSpikes_Real);
 			
-		[pks_Real,locs_Real,w_Real,p_Real] = findpeaks(vecStimIFR_Real);
+		[vecPeakHeight_Real,vecPeakLocs_Real,w_Real,p_Real] = findpeaks(vecStimIFR_Real);
 		
 		%% plot
 		vecBinWE = 0:20;
@@ -274,9 +198,9 @@ for intRec=1:numel(sAggStim) %19 || weird: 11
 		%height
 		vecBinEH = -0.5:0.1:2;
 		vecBinPH = vecBinEH(2:end) + mean(diff(vecBinEH))/2;
-		vecHC_Real = histcounts(pks_Real,vecBinEH);
+		vecHC_Real = histcounts(vecPeakHeight_Real,vecBinEH);
 		vecHC_Real = vecHC_Real./sum(vecHC_Real(:));
-		vecHC_Shuff = histcounts(pks_Shuff,vecBinEH);
+		vecHC_Shuff = histcounts(vecPeakHeight_Shuff,vecBinEH);
 		vecHC_Shuff = vecHC_Shuff./sum(vecHC_Shuff(:));
 		intRemIdx = find(vecBinPH>0 & (vecHC_Real==0 | vecHC_Shuff==0),1);
 		if ~isempty(intRemIdx)
@@ -287,15 +211,16 @@ for intRec=1:numel(sAggStim) %19 || weird: 11
 			vecBinPH(indRem) = [];
 		end
 		%consider only positive peaks
-		pks_RealPos = pks_Real(pks_Real>0);
-		pks_ShuffPos = pks_Shuff(pks_Shuff>0);
+		pks_RealPos = vecPeakHeight_Real(vecPeakHeight_Real>0);
+		pks_ShuffPos = vecPeakHeight_Shuff(vecPeakHeight_Shuff>0);
 		vecRatio = (vecHC_Real-vecHC_Shuff)./(vecHC_Real+vecHC_Shuff);
 		intCutOff = find(vecBinPH>0 & (vecRatio>0 & [vecRatio(2:end)>0 false]),1);
 		if isempty(intCutOff) || mean(pks_RealPos) < mean(pks_ShuffPos)
 			strTit = sprintf('No cut-off; pos mu real=%.3f,shuff=%.3f',mean(pks_RealPos) ,mean(pks_ShuffPos));
+			dblCutOff = nan;
 		else
 			dblCutOff = vecBinPH(intCutOff);
-			strTit = sprintf('Cut-off = %.2f; including %.0f%%',dblCutOff,(sum(pks_Real>dblCutOff)/numel(pks_Real))*100);
+			strTit = sprintf('Cut-off = %.2f; including %.0f%%',dblCutOff,(sum(vecPeakHeight_Real>dblCutOff)/numel(vecPeakHeight_Real))*100);
 		end
 		
 		
@@ -309,58 +234,30 @@ for intRec=1:numel(sAggStim) %19 || weird: 11
 		plot(vecBinPH,vecRatio);
 		title(strTit,'interpreter','none');
 		
-		continue;
-		%% scatter properties of peaks
-		%figure
-		%scatter(pks_Real,p_Real)
-		%hold on
-		%scatter(pks_Shuff,p_Shuff)
-		
-		
-		%% algo #2
-		%vecStimIFR_Shuff = vecIFR_Shuff(indStimSpikes_Shuff);
-		%vecStimTime_Shuff = vecTime_Shuff(indStimSpikes_Shuff);
-		
-		%real
-		intLag = round((numel(vecStimIFR_Real)/numel(vecOrigStimOnTime))/2);
-			dblThreshZ = 1.5;
-			dblInfluence = 0.05;
-			[signals,avgFilter_Real,stdFilter_Real] = detectpeaks(vecStimIFR_Real,intLag,dblThreshZ,dblInfluence);
-			vecPeakLocs = find(signals==1);
-			figure
-			subplot(2,3,1)
-			plot(vecStimIFR_Real)
-			
-			subplot(2,3,2)
-			scatter(vecPeakLocs,vecStimIFR_Real(vecPeakLocs),'gx')
-			hold on
-			plot(avgFilter_Real)
-			plot(avgFilter_Real+stdFilter_Real)
-			
-			subplot(2,3,3)
-			vecISN_Real = (vecStimIFR_Real - avgFilter_Real)./stdFilter_Real;
-			
-			plot(vecISN_Real)
-			
-			%shuff
-			intLag = round((numel(vecStimIFR_Shuff)/numel(vecOrigStimOnTime))/2);
-			[signals,avgFilter_Shuff,stdFilter_Shuff] = detectpeaks(vecStimIFR_Shuff,intLag,dblThreshZ,dblInfluence);
-			vecPeakLocs = find(signals==1);
-			
-			subplot(2,3,4)
-			plot(vecStimIFR_Shuff)
-			
-			subplot(2,3,5)
-			scatter(vecPeakLocs,vecStimIFR_Shuff(vecPeakLocs),'gx')
-			hold on
-			plot(avgFilter_Shuff)
-			plot(avgFilter_Shuff+stdFilter_Shuff)
-			
-			subplot(2,3,6)
-			vecISN_Shuff = (vecStimIFR_Shuff - avgFilter_Shuff)./stdFilter_Shuff;
-			
-			plot(vecISN_Shuff)
-		return
+		%% save intermediate data
+		save(fullpath(strTargetDataPath,sprintf('T0Data_%s',strRec)),...
+			...%real
+			'vecAllSpikeTime_Real',...
+			'vecAllSpikeNeuron_Real',...
+			'vecTime_Real',...
+			'vecIFR_Real',...
+			...%shuffled
+			'vecAllSpikeTime_Shuff',...
+			'vecAllSpikeNeuron_Shuff',...
+			'vecTime_Shuff',...
+			'vecIFR_Shuff',...
+			...%filter real
+			'vecNormIFR_Real',...
+			'vecNormTime_Real',...
+			...%filter shuff
+			'vecNormIFR_Shuff',...
+			'vecNormTime_Shuff',...
+			...%peaks
+			'dblCutOff',...
+			'vecPeakHeight_Real',...
+			'vecPeakHeight_Shuff',...
+			'vecPeakLocs_Real',...
+			'vecPeakLocs_Shuff');
 	end
 end
 toc

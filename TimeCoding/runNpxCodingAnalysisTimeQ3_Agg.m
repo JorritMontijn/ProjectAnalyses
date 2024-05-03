a1d67150138cdeea9d24e21c53545b037b27184c
Jorrit Montijn <jorritmontijn@gmail.com>
2024-05-03 14:02:46 +0200

    Update runNpxCodingAnalysisTimeQ3_Agg.m

--- TimeCoding/runNpxCodingAnalysisTimeQ3_Agg.m
+++ TimeCoding/runNpxCodingAnalysisTimeQ3_Agg.m
@@ -7,10 +7,9 @@ cellDataTypes = {'Npx','Sim','ABI','SWN'};%topo, model, allen, nora
 intRunDataType = 1;
 strRunStim = 'DG';%DG or NM? => superseded to WS by SWN
 dblRemOnset = 0; %remove onset period in seconds; 0.125 for sim, 0.25 for npx
-cellTypes = {'Real','Poiss','ShuffTid','RandTid','RandTxClass','Uniform'};
+cellTypes = {'Real'};%{'Real','Poiss','ShuffTid','RandTid','RandTxClass','Uniform'};
 runHeaderPopTimeCoding;
 boolMakeFigs = true;
-%vecTimescales = 0.01:0.01:10;%10;1.5;
 vecTimescales = [1e-2 1e-1 1e-0]; %timescales for rapid flucts, noise corrs, tuning
 vecJitter = [0 1e3];%[0 (2.^(-9:10))];
 intPopSize = inf; %24 (smallest pop of all recs) or inf (uses full pop for each rec)
@@ -101,6 +100,9 @@ for intRec=1:intRecNum %19 || weird: 11
 		intTimescaleNum = numel(vecTimescales);
 		intNumN = numel(cellOrigSpikeTimes);
 		cellSpikeTimes = cell(1,intNumN);
+		dblTotDur=dblStimDur*numel(vecOrigStimOnTime);
+		dblStart = 0;
+		dblEnd = dblTotDur;
 		vecBinNumPerTimescale = floor(dblTotDur./vecTimescales);
 		matMean = nan(intNumN,intTimescaleNum);
 		matSd = nan(intNumN,intTimescaleNum);
@@ -110,9 +112,6 @@ for intRec=1:intRecNum %19 || weird: 11
 		for i=1:intNumN
 			[vecPseudoSpikeTimes,vecPseudoStartT] = getPseudoSpikeVectors(cellOrigSpikeTimes{i},vecOrigStimOnTime,dblStimDur,true);
 			
-			dblStart = vecPseudoStartT(1);
-			dblEnd = vecPseudoStartT(end)+dblStimDur;
-			dblTotDur = dblEnd-dblStart;
 			vecPseudoSpikeTimes(vecPseudoSpikeTimes<dblStart | vecPseudoSpikeTimes>dblEnd) = [];
 			vecPseudoSpikeTimes = vecPseudoSpikeTimes-dblStart;
 			dblLastSpike = max(vecPseudoSpikeTimes);
@@ -131,7 +130,11 @@ for intRec=1:intRecNum %19 || weird: 11
 		end
 		
 		%% analysis
+		close all;
+		clear sData;
 		for intScale=1:intTimescaleNum
+			%%
+			dblTimescale = vecTimescales(intScale);
 			matCounts = cellCounts{intScale};
 			[intNumN,intNumT] = size(matCounts);
 			vecRealMean = matMean(:,intScale);
@@ -143,129 +146,165 @@ for intRec=1:intRecNum %19 || weird: 11
 			%gain-scaling axis?
 			
 			
-			
-			
-			%% fit gaussian model and compare real distro to expected distro (q-q plot)
-			%what do we want?
+			%% define source parameters
 			vecReqMu = vecRealMean;
 			vecReqSd = vecRealSd;
 			vecReqVar = vecReqSd.^2;
+			matReqCov = diag(vecReqVar);
 			
-			%log normal
-			vecSd = sqrt(log((vecReqSd.^2)./(vecReqMu.^2)+1));
-			vecMu = log(vecReqMu) - 0.5*(vecSd.^2);
-			vecVar = vecSd.^2;
-			matCovar = diag(vecVar);
-			
-			%expectation for exp(mvnrand):
-			vecExpectedMu = exp(vecMu + 0.5*vecVar);
-			matExpectedVar = exp(vecMu + vecMu' + 0.5*(vecVar + vecVar'))*(exp(matCovar)-1);
-			matExpectedSd = sqrt(matExpectedVar);
-			vecExpectedSd = diag(matExpectedSd);
+			%% fit gaussian model and compare real distro to expected distro (q-q plot)
+			%multi-variate gauss; 2*n free params: vecMean, vecSd
 			
 			%example gauss; 2*n free params: vecMu, vecVar
-			matCountsGauss = exp(mvnrnd(vecMu,matCovar,intNumT));
+			matCountsGauss = logmvnrnd(vecReqMu,matReqCov,intNumT)';
 			
-			%% fit gain-scaling model and compare real
-			%example gain; n+3 free params: vecMu, dblCV, dblRange
-			vecGainAxis = vecReqMu;
+			%% fit simple gain-scaling model
+			%gain with no off-axis noise; n+2 free params: vecGainAxis, dblGainMean, dblGainSd
+			vecGainAxis = vecReqMu/norm(vecReqMu);
 			[vecProjectedLocation,matProjectedPoints,vecProjLocDimNorm] = getProjOnLine(matCounts,vecGainAxis);
 			dblGainRange = std(vecProjectedLocation);
-			dblGainMean = sqrt(sum(mean(matCounts,1).^2));
+			dblGainMean = mean(vecProjectedLocation);
+			
+			%generate random gains
+			vecPopGainPerTrial = logmvnrnd(dblGainMean,dblGainRange^2,intNumT);
+			
+			%prediction is on-axis gain
+			matCountsGain = nan(size(matCountsGauss));
+			for intT = 1:intNumT
+				dblThisGain = vecPopGainPerTrial(intT);
+				vecOnAxisAct = vecGainAxis * dblThisGain;
+				matCountsGain(:,intT) = vecOnAxisAct;
+			end
+			
+			%% fit gain-scaling model
+			%gain with off-axis noise; n+2 free params: vecGainAxis, dblGainMean, dblGainSd
+			[vecRealS,vecReorder] = sort(sum(matCounts,1));
 			matResiduals = matProjectedPoints-matCounts;
 			vecOffAxis = sqrt(sum(matResiduals.^2));
-			vecCV = vecOffAxis'./vecProjectedLocation;
-			dblCV = 0;%mean(vecCV);
-			%dblGainMean1 = mean(vecProjectedLocation);
-			%dblGainMean2 = mean(sqrt(sum(matActGauss.^2,2)));
-			vecRandPopGainPerTrial = normrnd(dblGainMean,dblGainRange,[1 intNumT]);
+			vecCV = vecOffAxis'./(vecProjectedLocation+eps);
+			dblCV = median(vecCV);
 			
-			%decompose sd into elements for each neuron
-			matCountsGain = nan(size(matActGauss));
+			matScaledResid = (matResiduals./(vecProjectedLocation+eps)');
+			dblResidWidth = std(matScaledResid(:));
+			
+			%prediction is on-axis gain plus off-axis noise
+			matCountsGainFull = nan(size(matCountsGauss));
 			for intT = 1:intNumT
-				dblThisGain = vecRandPopGainPerTrial(intT);
-				vecOnAxis =  vecGainAxis.*dblThisGain/dblGainMean;
-				vecOffAxis = dblCV*dblThisGain/dblGainMean;
+				dblThisGain = vecPopGainPerTrial(intT);
+				vecOnAxisAct = vecGainAxis * dblThisGain;
 				
-				%get lognormal
-				vecSd = sqrt(log((vecOffAxis.^2)./(vecOnAxis.^2)+1));
-				vecMu = log(vecOnAxis) - 0.5*(vecOffAxis.^2);
-				vecVar = vecSd.^2;
-				matCovar = diag(vecVar);
+				%create "residuals"
+				vecOffAxisScale = dblCV * vecOnAxisAct;
+				vecRandOffset = normrnd(0,dblCV,size(vecOnAxisAct));
+				vecOffAxisAct = vecOffAxisScale.*vecRandOffset;
 				
-				%matCountsGain(intT,:) = exp(mvnrnd(vecMu,matCovar,1));
-				matCountsGain(intT,:) = mvnrnd(vecOnAxis,0*matCovar,1);
-				
-				%project on gain axis and set to
-				% 		vecRandNormalPerNeuron = randn(size(vecGainAxis));
-				% 		vecRandOnAxis = vecGainAxis.*vecRandPopGainPerTrial(intT);
-				% 		dblRandSd = abs(normrnd(0,dblGainRange));
-				% 		vecRandOffAxis = vecRandOnAxis.*dblRandSd.*vecRandNormalPerNeuron;%(vecRandNormalPerNeuron./norm(vecRandNormalPerNeuron));
-				% 		vecTotAct = vecRandOnAxis+vecRandOffAxis;
-				% 		matActGain(intT,:) = vecTotAct;
+				matCountsGainFull(:,intT) = max(0,vecOnAxisAct+vecOffAxisAct);
 			end
-			% q-q plot
-			figure
-			subplot(2,3,1)
-			vecRealS = sort(matCounts(:));
-			vecGaussS = sort(matCountsGauss(:));
-			vecGainS = sort(matCountsGain(:));
-			vecRealQ = cumsum(vecRealS)./sum(vecRealS);
-			vecGaussQ = cumsum(vecGaussS)./sum(vecGaussS);
-			vecGainQ = cumsum(vecGainS)./sum(vecGainS);
 			
-			scatter(vecRealQ,vecGaussQ,'.')
-			hold on
-			plot([0 1],[0 1],'k--');
-			ylim([0 1]);
+			%% plot
+			%get statistics
+			vecRealS = sort(sum(matCounts,1));
+			vecGaussS = sort(sum(matCountsGauss,1));
+			vecGainS = sort(sum(matCountsGain,1));
+			vecGainFullS = sort(sum(matCountsGainFull,1));
+			dblMaxLim = max(cat(1,vecRealS(:),vecGaussS(:),vecGainS(:),vecGainFullS(:)));
 			
-			subplot(2,3,2)
-			scatter(vecRealQ,vecGainQ,'.')
-			hold on
-			plot([0 1],[0 1],'k--');
-			ylim([0 1]);
+			dblR2_Gauss = getR2(vecRealS,vecGaussS);
+			dblR2_Gain = getR2(vecRealS,vecGainS);
+			dblR2_GainFull = getR2(vecRealS,vecGainFullS);
 			
+			vecRealS_Single = sort(matCounts(:));
+			vecGaussS_Single = sort(matCountsGauss(:));
+			vecGainS_Single = sort(matCountsGain(:));
+			vecGainFullS_Single = sort(matCountsGainFull(:));
+			dblMaxLim_Single = max(cat(1,vecRealS_Single(:),vecGaussS_Single(:),vecGainS_Single(:),vecGainFullS_Single(:)));
+			
+			dblR2_Gauss_Single = getR2(vecRealS_Single,vecGaussS_Single);
+			dblR2_Gain_Single = getR2(vecRealS_Single,vecGainS_Single);
+			dblR2_GainFull_Single = getR2(vecRealS_Single,vecGainFullS_Single);
+			
+			if boolMakeFigs
+				% q-q plot
+				figure;maxfig;
+				subplot(2,3,1)
+				scatter(vecRealS,vecGaussS,'.')
+				hold on
+				plot([0 dblMaxLim],[0 dblMaxLim],'k--');
+				xlabel('Real pop spike count')
+				ylabel('Mvn model pop spike count')
+				title(sprintf('Sorted distro, t=%.3fs; Gauss R^2=%.3f',dblTimescale,dblR2_Gauss));
+				
+				subplot(2,3,2)
+				scatter(vecRealS,vecGainS,'.')
+				hold on
+				plot([0 dblMaxLim],[0 dblMaxLim],'k--');
+				xlabel('Real pop spike count')
+				ylabel('Gain-only model pop spike count')
+				title(sprintf('%s; Gain R^2=%.3f',strRec,dblR2_Gain),'interpreter','none');
+				
+				subplot(2,3,3)
+				scatter(vecRealS,vecGainFullS,'.')
+				hold on
+				plot([0 dblMaxLim],[0 dblMaxLim],'k--');
+				xlabel('Real pop spike count')
+				ylabel('Full gain model pop spike count')
+				title(sprintf('Sorted distro; Full gain R^2=%.3f',dblR2_GainFull));
+				
+				% single neurons
+				subplot(2,3,4)
+				if numel(vecRealS_Single)>1000
+					vecPlot=round(linspace(1,numel(vecRealS_Single),1000));
+				else
+					vecPlot=1:numel(vecRealS_Single);
+				end
+				
+				scatter(vecRealS_Single(vecPlot),vecGaussS_Single(vecPlot),'.')
+				hold on
+				plot([0 dblMaxLim_Single],[0 dblMaxLim_Single],'k--');
+				xlabel('Real single neuron spike count')
+				ylabel('Mvn model single neuron spike count')
+				title(sprintf('Sorted distro; Gauss R^2=%.3f',dblR2_Gauss_Single));
+				
+				subplot(2,3,5)
+				scatter(vecRealS_Single(vecPlot),vecGainS_Single(vecPlot),'.')
+				hold on
+				plot([0 dblMaxLim_Single],[0 dblMaxLim_Single],'k--');
+				xlabel('Real single neuron spike count')
+				ylabel('Gain only model single neuron spike count')
+				title(sprintf('Sorted distro; Gain R^2=%.3f',dblR2_Gain_Single));
+				
+				subplot(2,3,6)
+				scatter(vecRealS_Single(vecPlot),vecGainFullS_Single(vecPlot),'.')
+				hold on
+				plot([0 dblMaxLim_Single],[0 dblMaxLim_Single],'k--');
+				xlabel('Real single neuron spike count')
+				ylabel('Full gain model single neuron spike count')
+				title(sprintf('Sorted distro; Full gain R^2=%.3f',dblR2_GainFull_Single));
+				fixfig;
+				
+				%% save figure
+				export_fig(fullpath(strFigurePathSR,sprintf('Q3A_GaussGainPrediction_%s_%s_T%d_%s.tif',strThisRec,strType,intScale,strOnset)));
+				export_fig(fullpath(strFigurePathSR,sprintf('Q3A_GaussGainPrediction_%s_%s_T%d_%s.pdf',strThisRec,strType,intScale,strOnset)));
+			end
+			%save data
+			sData(intTimescaleNum).strRec = strThisRec;
+			sData(intTimescaleNum).dblTimescale = dblTimescale;
+			sData(intTimescaleNum).strType = strType;
+			sData(intTimescaleNum).strOnset = strOnset;
+			sData(intTimescaleNum).dblR2_Gauss = dblR2_Gauss;
+			sData(intTimescaleNum).dblR2_Gain = dblR2_Gain;
+			sData(intTimescaleNum).dblR2_GainFull = dblR2_GainFull;
+			sData(intTimescaleNum).dblR2_Gauss_Single = dblR2_Gauss_Single;
+			sData(intTimescaleNum).dblR2_Gain_Single = dblR2_Gain_Single;
+			sData(intTimescaleNum).dblR2_GainFull_Single = dblR2_GainFull_Single;
 		end
-		%%
-		%check
 		
-		%example gain; n+3 free params: vecMu, dblCV, dblRange
-		[vecProjectedLocationG,matProjectedPointsG,vecProjLocDimNormG] = getProjOnLine(matCountsGain',vecGainAxis);
-		dblGainRangeG = std(vecProjectedLocationG)
-		dblGainMeanG = sqrt(sum(mean(matCountsGain,1).^2))
-		%dblGainMean1 = mean(vecProjectedLocation);
-		%dblGainMean2 = mean(sqrt(sum(matActGauss.^2,2)));
-		
-		matActGauss = matActGauss;
-		%matActGain = exp(matActGain);
-		
-		
-		
-		%plot
-		figure
-		subplot(2,3,1)
-		scatter(matActGauss(:,1),matActGauss(:,2),'.')
-		hold on
-		scatter(vecExpectedMu(1),vecExpectedMu(2),'kx');
-		hold off
-		xlim([0 5]);
-		ylim([0 5]);
-		set(gca,'xscale','log','yscale','log');
-		
-		subplot(2,3,2)
-		scatter(matCountsGain(:,1),matCountsGain(:,2),'.')
-		hold on
-		plot([0 norm(vecReqMu)*vecGainAxis(1)],[0 norm(vecReqMu)*vecGainAxis(2)],'k');
-		hold off
-		xlim([0 5]);
-		ylim([0 5]);
-		set(gca,'xscale','log','yscale','log');
-		return
+		%% agg data
+		eval(['sAggData.sData' strType ' = sData;']);
 	end
-	return
+	
 	%% save agg data
 	save(fullpath(strTargetDataPath,sprintf('Q3Data%s_%s.mat',strThisRec,strOnset)),...
 		'sAggData');
-	close all;
 end
 toc
